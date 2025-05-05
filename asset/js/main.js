690c1eb0107e915658a3986ed12cf61a76569707
Octafad <rizkioctafadilah96@gmail.com>
2025-05-05 18:14:55 +0700

    Add files via upload

---
+++ asset/js/main.js
@@ -0,0 +1,15 @@
+function searchFunction() {
+    let value = document.getElementById("inputSearch").value;
+    let ul = document.querySelector(".content_list");
+    let li = ul.getElementsByTagName("li");
+
+    for( let i = 0; i < li.length; i++) {
+        let a = li[i].getElementsByTagName("a")[0];
+        
+        if(a.innerHTML.toUpperCase().indexOf(value.toUpperCase()) > -1) {
+            li[i].style.display = "block";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
