8296e0aca951ad4c806fca6b4f033d32819028ae
Rizki <rizkioctafadilah96@gmail.com>
2025-06-09 20:32:36 +0700

    Add files via upload

---
+++ asset/script/header.js
@@ -0,0 +1,22 @@
+// ===========================================================================================================
+// header title
+const headerTitle = document.getElementById ("header-title");
+headerTitle.textContent = "Oarload";
+
+// ===========================================================================================================
+// menu item
+const menuItem1 = document.getElementById ("menu-item1");
+menuItem1.textContent = "Beranda";
+menuItem1.setAttribute ("href", "/index.html");
+
+const menuItem2 = document.getElementById ("menu-item2");
+menuItem2.textContent = "C++";
+menuItem2.setAttribute ("href", "/page/page-content.html");
+
+const menuItem3 = document.getElementById ("menu-item3");
+menuItem3.textContent = "Animasi";
+menuItem3.setAttribute ("href", "/page/page-content.html");
+
+const menuItem4 = document.getElementById ("menu-item4");
+menuItem4.textContent = "Tentang";
+menuItem4.setAttribute ("href", "/page/page-content.html");
